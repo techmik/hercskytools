d169e6c6613397efe2758866ff58f87d80e82b85
David Ferguson <ferguson.david@gmail.com>
2012-07-18 16:20:20 -0400

    updated

--- cm9-skyrocket-apply.sh
+++ cm9-skyrocket-apply.sh
@@ -52,23 +52,55 @@ set -e
 
 ################ Apply Patches Below ####################
 
-repo start auto device/samsung/skyrocket
-cdv device/samsung/skyrocket
-echo "### skyrocket: enable compbypass"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_skyrocket refs/changes/99/18999/2 && git cherry-pick FETCH_HEAD
+
+repo start auto sdk
+cdv sdk
+echo "### Changed make files to allow compiling in OS X 10.8 - Mountain Lion"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_sdk refs/changes/87/19487/1 && git cherry-pick FETCH_HEAD
 cdb
 
-repo start auto kernel/samsung/msm8660-common
-cdv kernel/samsung/msm8660-common
-echo "### imported I727 kernel source"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/00/19000/1 && git cherry-pick FETCH_HEAD
+repo start auto external/qemu
+cdv external/qemu
+echo "### Changed make files to allow compiling in OS X 10.8 - Mountain Lion"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_external_qemu refs/changes/86/19486/1 && git cherry-pick FETCH_HEAD
 cdb
 
-repo start auto kernel/samsung/msm8660-common
-cdv kernel/samsung/msm8660-common
-echo "### skyrocket: update defconfig with config from UCLF6"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/01/19001/1 && git cherry-pick FETCH_HEAD
+repo start auto hardware/qcom/display
+cdv hardware/qcom/display
+echo "### overlay: Allow kernel-controlled rotator formats"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_display refs/changes/57/19557/1 && git cherry-pick FETCH_HEAD
 cdb
+
+repo start auto device/samsung/msm8660-common
+cdv device/samsung/msm8660-common
+echo "### msm8660: set QCOM_ACDB_ENABLED for proper audio handling"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/60/19360/1 && git cherry-pick FETCH_HEAD
+cdb
+
+repo start auto hardware/qcom/audio
+cdv hardware/qcom/audio
+echo "### audio/msm8660: define missing INVALID_ACDB_ID"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/61/19361/2 && git cherry-pick FETCH_HEAD
+cdb
+
+repo start auto hardware/qcom/audio
+cdv hardware/qcom/audio
+echo "### audio/msm8660: add support for Samsung T989/I727"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/46/19346/3 && git cherry-pick FETCH_HEAD
+cdb
+
+repo start auto hardware/qcom/audio
+cdv hardware/qcom/audio
+echo "### msm8660: Add support for Samsung voice devices"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/10/19410/1 && git cherry-pick FETCH_HEAD
+cdb
+
+repo start auto hardware/qcom/audio
+cdv hardware/qcom/audio
+echo "### audio:msm8660: Fix for NT mode encode in audio HAL"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/78/19378/1 && git cherry-pick FETCH_HEAD
+cdb
+
 
 ##### SUCCESS ####
 SUCCESS=true
