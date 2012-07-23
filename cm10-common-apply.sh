485e8188f8bb3ca9bfd8454389067d4d1b3373c9
David Ferguson <ferguson.david@gmail.com>
2012-07-23 18:57:12 -0400

    updated

--- cm10-common-apply.sh
+++ cm10-common-apply.sh
@@ -26,23 +26,3 @@ echo "### camera: Add parameters for some Samsung cameras"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_av refs/changes/21/19821/1 && git cherry-pick FETCH_HEAD
 cdb
 
-repo start auto hardware/qcom/audio
-cdv hardware/qcom/audio
-echo "### audio:msm8660: Fix for NT mode encode in audio HAL"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/78/19378/1 && git cherry-pick FETCH_HEAD
-echo "### audio/msm8660: add support for Samsung T989/I727"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/46/19346/3 && git cherry-pick FETCH_HEAD
-echo "### msm8660: Add support for Samsung voice devices"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/10/19410/1 && git cherry-pick FETCH_HEAD
-echo "### msm8660: Use VR channels on Samsung devices"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/37/19637/2 && git cherry-pick FETCH_HEAD
-echo "### msm8660: Change fallback route to speaker"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/39/19639/1 && git cherry-pick FETCH_HEAD
-echo "### msm8660: Add samsung dualmic support"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/40/19640/1 && git cherry-pick FETCH_HEAD
-echo "### msm8660: Add support for camcorder mic"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/41/19641/2 && git cherry-pick FETCH_HEAD
-echo "### audio/msm8660: Add support for sony camcorder mic"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/42/19642/1 && git cherry-pick FETCH_HEAD
-cdb
-
