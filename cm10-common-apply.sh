b8af97b7f2a93e64c930f24f64cb6243247fd7a0
David Ferguson <ferguson.david@gmail.com>
2012-07-24 15:29:26 -0400

    updates

--- cm10-common-apply.sh
+++ cm10-common-apply.sh
@@ -26,3 +26,23 @@ echo "### camera: Add parameters for some Samsung cameras"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_av refs/changes/21/19821/1 && git cherry-pick FETCH_HEAD
 cdb
 
+repo start auto hardware/qcom/audio
+cdv hardware/qcom/audio
+echo "### audio/msm8660: add compatibility for the older voice api"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/46/20046/1 && git cherry-pick FETCH_HEAD
+cdb
+
+#repo start auto device/samsung/msm8660-common
+#cdv device/samsung/msm8660-common
+#echo "### Reverting: bln: new driver with kernel blink and rate support [1/2]"
+#git revert -n b2ff6ec11e9a4c50517491347ed3685d53450e44
+#git commit -m "reverting b2ff6ec11e9a4c50517491347ed3685d53450e44"
+#cdb
+
+#repo start auto kernel/samsung/msm8660-common
+#cdv kernel/samsung/msm8660-common
+#echo "### Reverting: bln: new driver with kernel blink and rate support [2/2]"
+#git revert -n 7dadd406832acf292477881c4e779a3ba6eb0255
+#git commit -m "reverting 7dadd406832acf292477881c4e779a3ba6eb0255"
+#cdb
+
