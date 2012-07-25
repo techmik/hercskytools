57016a01be22a5179256ee3833bf1cee9a72c169
David Ferguson <ferguson.david@gmail.com>
2012-07-25 19:14:37 -0400

    updates

--- cm10-common-apply.sh
+++ cm10-common-apply.sh
@@ -14,6 +14,12 @@ echo "### audio/msm8660: add compatibility for the older voice api"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/46/20046/2 && git cherry-pick FETCH_HEAD
 cdb
 
+repo start auto frameworks/av
+cdv frameworks/av
+echo "### ACodec: Video bringup for NuPlayer and other fixes"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_av refs/changes/11/19411/7 && git cherry-pick FETCH_HEAD
+cdb
+
 #repo start auto device/samsung/msm8660-common
 #cdv device/samsung/msm8660-common
 #echo "### Reverting: bln: new driver with kernel blink and rate support [1/2]"
