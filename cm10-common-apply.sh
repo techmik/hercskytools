679a7e555c5779ebca5ba5f4edcc94728375ca53
David Ferguson <ferguson.david@gmail.com>
2012-07-25 06:32:20 -0400

    updates

--- cm10-common-apply.sh
+++ cm10-common-apply.sh
@@ -11,19 +11,13 @@ cdb
 repo start auto external/qemu
 cdv external/qemu
 echo "### Changed make files to allow compiling in OS X 10.8 - Mountain Lion"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_external_qemu refs/changes/86/19486/1 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_external_qemu refs/changes/86/19486/2 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto hardware/qcom/display
 cdv hardware/qcom/display
 echo "### overlay: Allow kernel-controlled rotator formats"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_display refs/changes/57/19557/1 && git cherry-pick FETCH_HEAD
-cdb
-
-repo start auto frameworks/av
-cdv frameworks/av
-echo "### camera: Add parameters for some Samsung cameras"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_av refs/changes/21/19821/1 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto hardware/qcom/audio
