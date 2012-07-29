7c5fef944810fb6b83fe43feb35d05e71ad31ad8
Mike Grissom <mikeyman77@gmail.com>
2012-07-28 17:49:22 -0700

    update

--- cm10-common-apply.sh
+++ cm10-common-apply.sh
@@ -2,12 +2,6 @@
 
 ################ Apply Patches Below ####################
 
-repo start auto hardware/qcom/audio
-cdv hardware/qcom/audio
-echo "### audio/msm8660: add compatibility for the older voice api"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/46/20046/2 && git cherry-pick FETCH_HEAD
-cdb
-
 repo start auto hardware/libhardware_legacy
 cdv hardware/libhardware_legacy
 echo "### hardware/libhardware_legacy: Add interface function for LPA and FM"
