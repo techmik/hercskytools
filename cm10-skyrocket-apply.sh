c66f5a5cc0e25c187078d87955ef4fa0ebf13edd
David Ferguson <ferguson.david@gmail.com>
2012-08-08 17:21:15 -0400

    update

--- cm10-skyrocket-apply.sh
+++ cm10-skyrocket-apply.sh
@@ -60,7 +60,7 @@ source $DIR/cm10-common-apply.sh
 repo start auto device/samsung/skyrocket
 cdv device/samsung/skyrocket
 echo "### skyrocket: enable compiling with LPA"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_skyrocket refs/changes/83/20183/1 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_skyrocket refs/changes/83/20183/2 && git cherry-pick FETCH_HEAD
 cdb
 
 ##### SUCCESS ####
