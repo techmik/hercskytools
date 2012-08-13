77603b7777bf5d58f32c10ecc7f8cc3a44e40270
David Ferguson <ferguson.david@gmail.com>
2012-08-13 14:01:33 -0400

    update

--- cm10-common-apply.sh
+++ cm10-common-apply.sh
@@ -7,7 +7,7 @@ cdv device/samsung/msm8660-common
 echo "### msm8660: msm8660: Update headers to match new kernelspace"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/60/21060/1 && git cherry-pick FETCH_HEAD
 echo "### msm8660: Enable HW VSYNC"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/61/21061/1 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/61/21061/3 && git cherry-pick FETCH_HEAD
 echo "### msm8660: Update media_codecs.xml"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/88/21088/3 && git cherry-pick FETCH_HEAD
 echo "### msm8660: use toolchain 4.4.3 for kernel compile"
