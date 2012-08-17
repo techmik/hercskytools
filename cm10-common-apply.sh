42c150140fbb7e48dcb85c9f9c5253ee8416cfb6
David Ferguson <ferguson.david@gmail.com>
2012-08-17 13:12:23 -0400

    update

--- cm10-common-apply.sh
+++ cm10-common-apply.sh
@@ -8,9 +8,3 @@ echo "### msm8660: enable mounting of both sdcards in recovery USB mass storage"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/30/21430/4 && git cherry-pick FETCH_HEAD
 cdb
 
-repo start auto kernel/samsung/msm8660-common
-cdv kernel/samsung/msm8660-common
-echo "### Merge in Samsung changes for SGH-T879"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/57/21457/1 && git cherry-pick FETCH_HEAD
-cdb
-
