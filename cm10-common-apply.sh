09bff8379fb2d1f57ea8c54bce7bc05467c28f28
David Ferguson <ferguson.david@gmail.com>
2012-08-17 08:10:43 -0400

    updates

--- cm10-common-apply.sh
+++ cm10-common-apply.sh
@@ -4,13 +4,13 @@
 
 repo start auto device/samsung/msm8660-common
 cdv device/samsung/msm8660-common
-echo "### msm8660: msm8660: Update headers to match new kernelspace"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/60/21060/1 && git cherry-pick FETCH_HEAD
-echo "### msm8660: Enable HW VSYNC"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/61/21061/3 && git cherry-pick FETCH_HEAD
-echo "### msm8660: Update media_codecs.xml"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/88/21088/3 && git cherry-pick FETCH_HEAD
-echo "### msm8660: use toolchain 4.4.3 for kernel compile"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/68/21168/1 && git cherry-pick FETCH_HEAD
+echo "### msm8660: enable mounting of both sdcards in recovery USB mass storage"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/30/21430/4 && git cherry-pick FETCH_HEAD
+cdb
+
+repo start auto kernel/samsung/msm8660-common
+cdv kernel/samsung/msm8660-common
+echo "### Merge in Samsung changes for SGH-T879"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/57/21457/1 && git cherry-pick FETCH_HEAD
 cdb
 
