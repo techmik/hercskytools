1d9ef65e4dc2b65fbd9f162289951281eb074528
David Ferguson <ferguson.david@gmail.com>
2012-08-19 19:41:56 -0400

    update

--- cm10-common-apply.sh
+++ cm10-common-apply.sh
@@ -2,9 +2,9 @@
 
 ################ Apply Patches Below ####################
 
-#repo start auto device/samsung/msm8660-common
-#cdv device/samsung/msm8660-common
-#echo "### msm8660: enable mounting of both sdcards in recovery USB mass storage"
-#git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/30/21430/4 && git cherry-pick FETCH_HEAD
-#cdb
+repo start auto frameworks/base
+cdv frameworks/base
+echo "### SamsungQualcommUiccRil: hack to prevent SIM unlock popup"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/19/21719/1 && git cherry-pick FETCH_HEAD
+cdb
 
