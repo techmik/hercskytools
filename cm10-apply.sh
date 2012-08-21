39974f7e057c733a9a5d1a5a38aa4f78e1d1817a
David Ferguson <ferguson.david@gmail.com>
2012-08-21 14:45:11 -0400

    updates

--- cm10-apply.sh
+++ cm10-apply.sh
@@ -53,18 +53,6 @@ set -e
 
 ################ Apply Common Patches Below ####################
 
-repo start auto frameworks/base
-cdv frameworks/base
-echo "### SamsungQualcommUiccRil: hack to prevent SIM unlock popup"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/19/21719/1 && git cherry-pick FETCH_HEAD
-cdb
-
-repo start auto system/core
-cdv system/core
-echo "### diag: Remove world read/write permissions from /dev/diag"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_system_core refs/changes/93/21793/2 && git cherry-pick FETCH_HEAD
-cdb
-
 repo start auto vendor/cm
 cdv vendor/cm
 echo "### cm: New boot animation from Cameron Behzadpour"
@@ -77,12 +65,6 @@ cdb
 if [ -e device/samsung/hercules ]; then
 repo start auto device/samsung/hercules
 cdv device/samsung/hercules
-echo "### hercules: remove deprecated qcomuiccstack property"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_hercules refs/changes/28/21728/1 && git cherry-pick FETCH_HEAD
-echo "### hercules: mount debugfs to fix Trace"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_hercules refs/changes/60/21760/1 && git cherry-pick FETCH_HEAD
-echo "### hercules: Use new qcom_diag group for RIL"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_hercules refs/changes/09/21809/1 && git cherry-pick FETCH_HEAD
 echo "### hercules: use new square boot animation"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_hercules refs/changes/10/21810/1 && git cherry-pick FETCH_HEAD
 cdb
@@ -94,12 +76,6 @@ fi
 if [ -e device/samsung/skyrocket ]; then
 repo start auto device/samsung/skyrocket
 cdv device/samsung/skyrocket
-echo "### skyrocket: remove deprecated qcomuiccstack property"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_skyrocket refs/changes/30/21730/1 && git cherry-pick FETCH_HEAD
-echo "### skyrocket: mount debugfs to fix Trace"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_skyrocket refs/changes/61/21761/1 && git cherry-pick FETCH_HEAD
-echo "### skyrocket: Use new qcom_diag group for RIL"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_skyrocket refs/changes/11/21811/1 && git cherry-pick FETCH_HEAD
 echo "### skyrocket: use new square boot animation"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_skyrocket refs/changes/12/21812/1 && git cherry-pick FETCH_HEAD
 cdb
