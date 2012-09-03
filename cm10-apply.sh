e76fa168c6004250bb8a4cc08b750a3f3fbd5f0b
mikeyman77 <mikeyman77@gmail.com>
2012-09-02 18:32:14 -0700

    update

--- cm10-apply.sh
+++ cm10-apply.sh
@@ -53,25 +53,19 @@ set -e
 
 ################ Apply Common Patches Below ####################
 
-repo start auto frameworks/base
-cdv frameworks/base
-echo "### RILs: Eliminate repeated overrides of responseOperatorInfos."
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/76/22476/2 && git cherry-pick FETCH_HEAD
-cdb
-
-#repo start auto kernel/samsung/msm8660-common
-#cdv kernel/samsung/msm8660-common
-#echo "### Revert \"video: msm: Add user-defined backlight scaling\""
-#git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/77/22377/1 && git cherry-pick FETCH_HEAD
+repo start auto kernel/samsung/msm8660-common
+cdv kernel/samsung/msm8660-common
+echo "### Revert \"video: msm: Add user-defined backlight scaling\""
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/77/22377/1 && git cherry-pick FETCH_HEAD
 #echo "### LCD: ld9040: increase the delay in powerup"
 #git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/72/22272/1 && git cherry-pick FETCH_HEAD
-#cdb
+cdb
 
-#repo start auto device/samsung/msm8660-common
-#cdv device/samsung/msm8660-common
-#echo "### msm8660: remove mdp_bl_scale_data from header file to match kernel"
-#git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/78/22378/1 && git cherry-pick FETCH_HEAD
-#cdb
+repo start auto device/samsung/msm8660-common
+cdv device/samsung/msm8660-common
+echo "### msm8660: remove mdp_bl_scale_data from header file to match kernel"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_msm8660-common refs/changes/78/22378/1 && git cherry-pick FETCH_HEAD
+cdb
 
 
 ################ Apply Hercules-Specific Patches Below ####################
