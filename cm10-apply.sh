6f6d7cf81d2b868147d2897d1b2025a059ef0a72
David Ferguson <ferguson.david@gmail.com>
2012-09-04 10:32:36 -0400

    update

--- cm10-apply.sh
+++ cm10-apply.sh
@@ -55,8 +55,6 @@ set -e
 
 repo start auto kernel/samsung/msm8660-common
 cdv kernel/samsung/msm8660-common
-echo "### Revert \"video: msm: Add user-defined backlight scaling\""
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/77/22377/1 && git cherry-pick FETCH_HEAD
 echo "### LCD: ld9040: increase the delay in powerup"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/72/22272/1 && git cherry-pick FETCH_HEAD
 cdb
