8516bbfdfed83ac4aa4dbe27acabd4481a4afb1e
David Ferguson <ferguson.david@gmail.com>
2012-10-03 09:12:42 -0400

    update

--- cm10-apply.sh
+++ cm10-apply.sh
@@ -54,6 +54,12 @@ set -e
 ################ Apply Common Patches Below ####################
 
 
+repo start auto hardware/qcom/audio
+cdv hardware/qcom/audio
+echo "### msm8660 audio: fix BT in-call routing on non-HTC phones"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_hardware_qcom_audio refs/changes/38/24238/1 && git cherry-pick FETCH_HEAD
+cdb
+
 repo start auto device/samsung/msm8660-common
 cdv device/samsung/msm8660-common
 echo "### msm8660: import latest power HAL from qcom-common tree"
@@ -70,30 +76,36 @@ echo "### defconfig: hercules: Enable more debugging options"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/77/23277/2 && git cherry-pick FETCH_HEAD
 #echo "### msm8660: fix BUG crash in fs/dcache.c (WIP)"
 #git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/46/24046/1 && git cherry-pick FETCH_HEAD
-echo "### Revert \"block: replace __getblk_slow misfix by grow_dev_page fix\""
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/63/24063/1 && git cherry-pick FETCH_HEAD
+#echo "### Revert \"block: replace __getblk_slow misfix by grow_dev_page fix\""
+#git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/63/24063/1 && git cherry-pick FETCH_HEAD
 echo "### qt602240_ts: add additional mutex protection"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/14/24114/1 && git cherry-pick FETCH_HEAD
 echo "### lcdc_ld9040: add additional mutex protection"
 git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/13/24113/1 && git cherry-pick FETCH_HEAD
+echo "### video: ld9040: Turn panel off after stop_drawing"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/72/23372/1 && git cherry-pick FETCH_HEAD
 cdb
 
 
 ################ Apply Hercules-Specific Patches Below ####################
 
-#if [ -e device/samsung/hercules ]; then
-#repo start auto device/samsung/hercules
-#cdv device/samsung/hercules
-#cdb
-#fi
+if [ -e device/samsung/hercules ]; then
+repo start auto device/samsung/hercules
+cdv device/samsung/hercules
+echo "### hercules: enable beam animation"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_hercules refs/changes/12/24112/1 && git cherry-pick FETCH_HEAD
+cdb
+fi
 
 ################ Apply Skyrocket-Specific Patches Below ####################
 
-#if [ -e device/samsung/skyrocket ]; then
-#repo start auto device/samsung/skyrocket
-#cdv device/samsung/skyrocket
-#cdb
-#fi
+if [ -e device/samsung/skyrocket ]; then
+repo start auto device/samsung/skyrocket
+cdv device/samsung/skyrocket
+echo "### skyrocket: enable beam animation"
+git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_samsung_skyrocket refs/changes/57/24057/1 && git cherry-pick FETCH_HEAD
+cdb
+fi
 
 
 ##### SUCCESS ####
