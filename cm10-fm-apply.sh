007283bba22f0a53f277a4af81b611c039487b7f
David Ferguson <ferguson.david@gmail.com>
2013-01-30 20:28:39 -0500

    update

--- cm10-fm-apply.sh
+++ cm10-fm-apply.sh
@@ -58,45 +58,10 @@ cdv build
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_build refs/changes/26/30226/1 && git cherry-pick FETCH_HEAD
 cdb
 
-repo start auto hardware/libhardware
-cdv hardware/libhardware
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_libhardware refs/changes/27/30227/1 && git cherry-pick FETCH_HEAD
-cdb
-
-repo start auto development
-cdv development
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_development refs/changes/63/30363/1 && git cherry-pick FETCH_HEAD
-cdb
-
 repo start auto frameworks/base
 cdv frameworks/base
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/90/30390/1 && git cherry-pick FETCH_HEAD
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/50/30350/3 && git cherry-pick FETCH_HEAD
-cdb
-
-repo start auto frameworks/native
-cdv frameworks/native
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_native refs/changes/91/30391/1 && git cherry-pick FETCH_HEAD
-cdb
-
-repo start auto frameworks/av
-cdv frameworks/av
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_av refs/changes/23/30223/4 && git cherry-pick FETCH_HEAD
-cdb
-
-repo start auto hardware/libhardware_legacy
-cdv hardware/libhardware_legacy
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_libhardware_legacy refs/changes/72/30972/2 && git cherry-pick FETCH_HEAD
-cdb
-
-repo start auto system/core
-cdv system/core
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_system_core refs/changes/25/30225/7 && git cherry-pick FETCH_HEAD
-cdb
-
-repo start auto hardware/qcom/audio
-cdv hardware/qcom/audio
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_hardware_qcom_audio refs/changes/63/31063/3 && git cherry-pick FETCH_HEAD
 cdb
 
 repo start auto device/samsung/celox-common
@@ -106,7 +71,7 @@ cdb
 
 repo start auto device/samsung/msm8660-common
 cdv device/samsung/msm8660-common
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_samsung_msm8660-common refs/changes/81/31081/1 && git cherry-pick FETCH_HEAD
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_samsung_msm8660-common refs/changes/81/31081/2 && git cherry-pick FETCH_HEAD
 cdb
 
 
