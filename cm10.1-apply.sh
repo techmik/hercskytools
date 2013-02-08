efee33c030b0b2745bc174629dc696a984d0b992
David Ferguson <ferguson.david@gmail.com>
2013-02-08 11:00:33 -0500

    update

--- cm10.1-apply.sh
+++ cm10.1-apply.sh
@@ -51,13 +51,32 @@ repo abandon auto
 set -e
 
 
+# Changelog
+
+# Known issues
+#   - Brightness controls do not work properly
+#   - Camcorder preview is very low resolution
+#   - Recovery external_sd is not available
+#   - Recovery "reboot system" and "reboot recovery" simply turns power off
+# Preview1.1
+#   - Includes cyanogen tuning of the powerHAL (possibly fixes: CPU spends most of time at 1512 and 384 && battery life is short)
+#   - Clean up of the msm8660-common device tree
+# Preview1
+#   - first CM10.1 release
+
 ################ Apply Common Patches Below ####################
 
-#repo start auto device/samsung/msm8660-common
-#cdv device/samsung/msm8660-common
-#echo "msm8660-common: cleanup device tree"
-#git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_samsung_msm8660-common refs/changes/58/31358/1 && git cherry-pick FETCH_HEAD
-#cdb
+repo start auto device/samsung/msm8660-common
+cdv device/samsung/msm8660-common
+echo "msm8660-common: cleanup device tree"
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_samsung_msm8660-common refs/changes/05/31605/1 && git cherry-pick FETCH_HEAD
+cdb
+
+repo start auto device/samsung/qcom-common
+cdv device/samsung/qcom-common
+echo "qcom-common: power: Tuning and cleanups"
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_device_samsung_qcom-common refs/changes/04/31604/4 && git cherry-pick FETCH_HEAD
+cdb
 
 repo start auto vendor/cm
 cdv vendor/cm
