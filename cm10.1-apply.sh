089af85da649cd287d0d8ec03406825ff2141cd1
Mike Grissom <mikeyman77@gmail.com>
2013-05-23 13:41:39 -0700

    update

--- cm10.1-apply.sh
+++ cm10.1-apply.sh
@@ -59,9 +59,11 @@ set -e
 
 ################ Apply Common Patches Below ####################
 
-#repo start auto kernel/samsung/msm8660-common
-#cdv kernel/samsung/msm8660-common
-#cdb
+repo start auto kernel/samsung/msm8660-common
+cdv kernel/samsung/msm8660-common
+echo "msm8660: disable CONFIG_GPIO_SX150X"
+git fetch http://review.cyanogenmod.org/CyanogenMod/android_kernel_samsung_msm8660-common refs/changes/93/42893/1 && git cherry-pick FETCH_HEAD
+cdb
 
 #repo start auto device/samsung/celox-common
 #cdv device/samsung/celox-common
