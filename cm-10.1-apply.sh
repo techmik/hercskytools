1508e888f2d95da58f9361d944c5510177955c97
David Ferguson <ferguson.david@gmail.com>
2013-06-07 18:07:52 -0400

    updat

--- cm-10.1-apply.sh
+++ cm-10.1-apply.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# Forward port of TKEY
-#CHANGES+="42895 "
-
 # Mutex protection in TKEY
 CHANGES+="37621 "
-
-# LCD code cleanup
-#CHANGES+="43673 "
-
-# LCD backlight/brightness lock
-#CHANGES+="43732 "
-
-# LDC debugging code
-#CHANGES+="43793 "
 
 # LCD disable late resume
 CHANGES+="43804 "
