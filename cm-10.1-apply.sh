4df894282652981b57e3c6e1da0d0540eb60065f
Mike Grissom <mikeyman77@gmail.com>
2013-06-13 11:31:33 -0700

    update

--- cm-10.1-apply.sh
+++ cm-10.1-apply.sh
@@ -6,11 +6,8 @@ CHANGES+=(37621)
 # GPIO config fix in TKEY
 CHANGES+=(42894)
 
-# LCD disable late resume
-#CHANGES+=(43804)
-
-# ld9040: fix late resume
-CHANGES+=(43984)
+# cpufreq: enforce governor to stay in sync across CPU cores
+CHANGES+=(43536)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
