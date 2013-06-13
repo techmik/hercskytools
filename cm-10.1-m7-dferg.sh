390fd1657080b7ff27e2d2d6d1c48fdb0abfaf6b
David Ferguson <ferguson.david@gmail.com>
2013-06-13 19:23:18 -0400

    update

--- cm-10.1-m7-dferg.sh
+++ cm-10.1-m7-dferg.sh
@@ -3,14 +3,32 @@
 # msm serial: bulk update from caf jb_2.5
 #CHANGES+=(43791)
 
-# bluesleep: update from jf kernel
-CHANGES+=(43687)
+# roger_n's Test: Incognito
+#framework: Run in Incognito Mode (WIP)
+CHANGES+=(44192)
+
+#settings: Run in Incognito Mode (WIP)
+CHANGES+=(44193)
+
+#contacts: Incognito mode support (WIP)
+CHANGES+=(44194)
+
+#telephony: Support for Incognito Mode (WIP)
+CHANGES+=(44240)
+
+#calendar: Incognito mode support
+CHANGES+=(44233)
+
 
 # m7-common: set amplifier configs based on audio mode
-CHANGES+=(38221 38256)
+# ** needs rebase **
+#CHANGES+=(38221 38256)
 
 # m7: Make the ringtone properly repeat
 CHANGES+=(43943)
+
+# bluesleep: update from jf kernel
+CHANGES+=(43687)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
