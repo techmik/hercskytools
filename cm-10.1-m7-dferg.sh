dc0a69bb0cee23c3b71cc8883d8ca663641b5d69
David Ferguson <ferguson.david@gmail.com>
2013-06-20 15:59:11 -0400

    update

--- cm-10.1-m7-dferg.sh
+++ cm-10.1-m7-dferg.sh
@@ -3,14 +3,11 @@
 # Repopick enhancements
 CHANGES+=(44336 44456 44457)
 
+# button sensitivity fix
+CHANGES+=(44487)
+
 # amp init sequence
 CHANGES+=(44392 38221 38256)
-
-# use CAF media variant
-CHANGES+=(44447)
-
-# bluesleep from tegra
-CHANGES+=(44423 44424 44425 44426 44427 44428 44429 44430 44431 44432 44433 44434)
 
 # Incognito Mode (WIP)
 CHANGES+=(44192 44193 44194 44240 44233)
