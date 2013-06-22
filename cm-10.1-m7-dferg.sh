9dc4a53f6bf1bd0a959cdba94abf7811fd74e946
David Ferguson <ferguson.david@gmail.com>
2013-06-22 07:56:59 -0400

    update

--- cm-10.1-m7-dferg.sh
+++ cm-10.1-m7-dferg.sh
@@ -3,11 +3,8 @@
 # Repopick enhancements
 CHANGES+=(44336 44456 44457)
 
-# button sensitivity fix
-CHANGES+=(44487)
-
 # amp init sequence
-CHANGES+=(44392 38221 38256)
+#CHANGES+=(44392 38221 38256)
 
 # Incognito Mode (WIP)
 CHANGES+=(44192 44193 44194 44240 44233)
