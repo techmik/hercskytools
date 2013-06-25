16d6ac4703320b2fbde32ef5932a7239e1a41670
Mike Grissom <mikeyman77@gmail.com>
2013-06-25 14:33:05 -0700

    update: add voip fixes for samsung devices

--- cm-10.1-apply.sh
+++ cm-10.1-apply.sh
@@ -12,6 +12,9 @@ CHANGES+=(42894)
 # cpufreq: enforce governor to stay in sync across CPU cores
 CHANGES+=(43536)
 
+# Voip fixes for Samsung
+CHANGES+=(44738 44740 44741)
+
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
 
