5f59e90c1ab3fc9fc1e47c8dcb84c24d5ee501e7
David Ferguson <ferguson.david@gmail.com>
2013-06-26 22:10:14 -0400

    update

--- cm-10.1-m7-dferg.sh
+++ cm-10.1-m7-dferg.sh
@@ -6,8 +6,8 @@ CHANGES+=(44336 44456 44457)
 # amp init sequence
 #CHANGES+=(44392 38221 38256)
 
-# Incognito Mode (WIP)
-CHANGES+=(44192 44193 44194 44240 44233)
+# BT
+CHANGES+=(44785)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
