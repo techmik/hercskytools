40a673c0acbfca657b339b78154809a673a53e03
David Ferguson <ferguson.david@gmail.com>
2013-06-28 07:52:02 -0400

    update

--- cm-10.1-m7-dferg.sh
+++ cm-10.1-m7-dferg.sh
@@ -3,14 +3,9 @@
 # Repopick enhancements
 CHANGES+=(44336 44456 44457)
 
-# amp init sequence
-#CHANGES+=(44392 38221 38256)
-
 # BT
+#  - breaks compiles at the moment
 #CHANGES+=(44785)
-
-# Google Edition kernel
-CHANGES+=(44819)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
