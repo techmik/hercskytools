e857da460be2b4f444a033164370d1b879a526bd
Mike Grissom <mikeyman77@gmail.com>
2013-06-30 17:20:19 -0700

    update

--- cm-10.1-apply.sh
+++ cm-10.1-apply.sh
@@ -13,7 +13,13 @@ CHANGES+=(42894)
 CHANGES+=(43536)
 
 # Voip fixes for Samsung
-CHANGES+=(44738 44962 44963 44964 44880)
+CHANGES+=(44738 44741 44962 44963 44964 44880 44981)
+
+# Changes for GCC 4.6
+CHANGES+=(44849 44728 44845)
+
+# Kconfig fix
+CHANGES+=(44979)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
