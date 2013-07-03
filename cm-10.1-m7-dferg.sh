bb3a2571f48714a309d29db5f294b568c2c6b2d9
David Ferguson <ferguson.david@gmail.com>
2013-07-03 10:42:00 -0400

    update

--- cm-10.1-m7-dferg.sh
+++ cm-10.1-m7-dferg.sh
@@ -3,10 +3,6 @@
 # Repopick enhancements
 CHANGES+=(44336 44456 44457)
 
-# BT
-#  - breaks compiles at the moment
-#CHANGES+=(44785)
-
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
 
