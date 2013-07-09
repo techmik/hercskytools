b70584181285a56a81f341cc454622f5e767a1a9
Mike Grissom <mikeyman77@gmail.com>
2013-07-08 20:44:39 -0700

    update

--- cm-10.1-apply.sh
+++ cm-10.1-apply.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Changes for GCC 4.6
-# - use 4.6 instead of 4.4.3
-CHANGES+=(44849)
 # - code clean ups
 CHANGES+=(44728)
 # - libpng: fix compiler warnings
@@ -13,9 +11,6 @@ CHANGES+=(44949)
 
 # Radio version check
 CHANGES+=(44984 44983 45029)
-
-# Voip fix
-CHANGES+=(45009)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
