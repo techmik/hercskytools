0cd2c4c5e74c5649ffec94fc762dda35eee8bd54
David Ferguson <ferguson.david@gmail.com>
2013-07-12 13:13:36 -0400

    update

--- cm-10.1-apply.sh
+++ cm-10.1-apply.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Kernel cleanups
-# - build optimizations for gcc4.6
-CHANGES+=(45324)
-
 # Misc cleanups
 # - libpng: fix compiler warnings
 CHANGES+=(44845)
@@ -11,8 +7,8 @@ CHANGES+=(44845)
 # CCACHE_BASEDIR override
 CHANGES+=(44949)
 
-# Radio version check
-CHANGES+=(44984 44983 45029)
+# Radio version check - Hercules
+CHANGES+=(44983)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
