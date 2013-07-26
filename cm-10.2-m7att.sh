336cba8d6eb8d0d7adc932399aa8d46828de4eec
RogerNn <noriega.roger@gmail.com>
2013-07-26 13:03:42 -0500

    Update cm-10.2-m7att.sh

--- cm-10.2-m7att.sh
+++ cm-10.2-m7att.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# SELinux
-CHANGES+=(45841)
+# Android Device common
+CHANGES+=(46066 46080 46081)
+
+#DashPlayer: make jb_mr2 compatible
+CHANGES+=(46086)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
