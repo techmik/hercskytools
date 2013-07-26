5050321511badde4bc38bce540885ce9994d5845
RogerNn <noriega.roger@gmail.com>
2013-07-26 14:14:01 -0500

    Update

--- cm-10.2-m7att.sh
+++ cm-10.2-m7att.sh
@@ -6,8 +6,5 @@ CHANGES+=(46066 46080 46081)
 # DashPlayer: make jb_mr2 compatible
 CHANGES+=(46086)
 
-# msm8960-common: add TARGET_CPU_VARIANT
-CHANGES+=(46079)
-
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
