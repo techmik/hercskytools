6420389a8921a038b48a12c90f0cbdd9734c94e0
RogerNn <noriega.roger@gmail.com>
2013-07-26 13:12:59 -0500

    Update

--- cm-10.2-m7att.sh
+++ cm-10.2-m7att.sh
@@ -3,8 +3,11 @@
 # Android Device common
 CHANGES+=(46066 46080 46081)
 
-#DashPlayer: make jb_mr2 compatible
+# DashPlayer: make jb_mr2 compatible
 CHANGES+=(46086)
+
+# msm8960-common: add TARGET_CPU_VARIANT
+CHANGES+=(46079)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
