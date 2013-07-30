fbede8b7149db068af2c8bb14c199b15966da4d7
David Ferguson <ferguson.david@gmail.com>
2013-07-30 07:54:05 -0400

    update

--- cm-10.2-celox.sh
+++ cm-10.2-celox.sh
@@ -15,9 +15,6 @@ CHANGES+=(46183)
 # gralloc: Add workaround for Jellybean camera drivers
 CHANGES+=(46310)
 
-# hwc: Add support for smooth streaming feature.
-CHANGES+=(46205)
-
 # mm-video: revert usage of flag GRALLOC_USAGE_PRIVATE_CP_BUFFER
 CHANGES+=(46276)
 
