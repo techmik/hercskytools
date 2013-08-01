280178a4ddf48ecf0b66ebb68494f97cc67c43d9
David Ferguson <ferguson.david@gmail.com>
2013-08-01 07:29:50 -0400

    update

--- cm-10.2-m7.sh
+++ cm-10.2-m7.sh
@@ -3,6 +3,12 @@
 # Auto brightness
 CHANGES+=(46609 46610)
 
+# Contact preloading
+CHANGES+=(46386)
+
+# Blacklist
+CHANGES+=(46382 46356 46359 46353 46354)
+
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
 
