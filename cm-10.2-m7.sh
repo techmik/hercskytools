2bfdd7001490f646810b1923dd6d35cea958617e
David Ferguson <ferguson.david@gmail.com>
2013-08-06 16:40:19 -0400

    update

--- cm-10.2-m7.sh
+++ cm-10.2-m7.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # BT kernel fixes
-CHANGES+=(47212 47217)
-
-# BT tethering, BT sleep bugs
-CHANGES+=(47234 47211)
+#CHANGES+=(47212 47217)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
