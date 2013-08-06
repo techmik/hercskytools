248169298804604dd14bd014a5f784cfd6bf346f
David Ferguson <ferguson.david@gmail.com>
2013-08-06 08:38:36 -0400

    update

--- cm-10.2-m7.sh
+++ cm-10.2-m7.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Auto brightness
-CHANGES+=(47064 46609 46610)
-
 # Contact preloading
 CHANGES+=(46386)
+
+# BT kernel fixes
+CHANGES+=(47212 47217)
+
+# BT tethering, BT sleep bugs
+CHANGES+=(47234 47211)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
