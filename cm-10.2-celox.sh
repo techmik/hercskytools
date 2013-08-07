c8153d1fa68c3e0404cb59bb8602ae2dfaa30dd4
Dave Carey <carvsdriver@gmail.com>
2013-08-07 13:07:12 -0400

    added kernel 3.0.89 patch

--- cm-10.2-celox.sh
+++ cm-10.2-celox.sh
@@ -41,6 +41,9 @@ CHANGES+=(46607)
 # qcom-common: enable hardware aac encoder
 CHANGES+=(47200)
 
+# patch kernel to version 3.0.89
+CHANGES+=(47262)
+
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
 
