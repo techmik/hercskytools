4c1883e084c2a8d52d618ed57adb30652b322e89
RogerNn <noriega.roger@gmail.com>
2013-08-13 21:28:31 -0500

    Update: 46625, 46287 merged.

--- cm-10.2-celox.sh
+++ cm-10.2-celox.sh
@@ -3,14 +3,8 @@
 # msm8660: enable Qualcomm AV enhancements
 CHANGES+=(46624)
 
-# msm8660: move Camera overlay and update
-CHANGES+=(46625)
-
 # msm8660: support Samsung extended AGPS
 CHANGES+=(46629)
-
-# Camera: Bring Samsung camera fixes and features to 4.3
-CHANGES+=(46287)
 
 # Add PR_{GET,SET}_NO_NEW_PRIVS to prevent execve from granting privs
 #CHANGES+=(47338)
