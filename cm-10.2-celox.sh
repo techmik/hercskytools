e9fe4fc399cc9170fe0939f7e0db61effb7b8aae
Mike Grissom <mikeyman77@gmail.com>
2013-08-15 13:17:10 -0700

    update

--- cm-10.2-celox.sh
+++ cm-10.2-celox.sh
@@ -6,13 +6,10 @@ CHANGES+=(46624)
 # msm8660: support Samsung extended AGPS
 CHANGES+=(46629)
 
-# Add PR_{GET,SET}_NO_NEW_PRIVS to prevent execve from granting privs
-#CHANGES+=(47338)
-
 # Focal overlay
 CHANGES+=(47432)
 
-# remove journal_async_commit and encryptable flag in fstab
+# remove journal_async_commit in fstab
 CHANGES+=(47856)
 
 # usbaudio: Make the USB audio HAL actually work.
