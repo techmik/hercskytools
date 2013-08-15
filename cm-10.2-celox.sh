26c225d657145a3b75c3f103c2da4f78734fe3cc
Mike Grissom <mikeyman77@gmail.com>
2013-08-15 13:34:48 -0700

    update

--- cm-10.2-celox.sh
+++ cm-10.2-celox.sh
@@ -12,12 +12,6 @@ CHANGES+=(47432)
 # remove journal_async_commit in fstab
 CHANGES+=(47856)
 
-# usbaudio: Make the USB audio HAL actually work.
-CHANGES+=(47948)
-
-# tinyalsa: Underp the period size parameter
-CHANGES+=(47946)
-
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
 
