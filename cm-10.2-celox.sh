e8514206f93fd24dd1bdb892d460e6e5d90feb9f
RogerNn <noriega.roger@gmail.com>
2013-08-24 20:50:32 -0500

    Update: removed merged picks.

--- cm-10.2-celox.sh
+++ cm-10.2-celox.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# msm8660: enable Qualcomm AV enhancements
-CHANGES+=(46624)
-
-# msm8660: support Samsung extended AGPS
-CHANGES+=(46629)
-
-# Add PR_{GET,SET}_NO_NEW_PRIVS 
-CHNAGES+=(47338)
-
 # Focal overlay
 CHANGES+=(47432)
+
+# repopick: fix broken pipe error from "repo list"
+CHANGES+=(48501)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
